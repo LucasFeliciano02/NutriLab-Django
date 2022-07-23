ea93cc1cf0df7bb5855ea9118ec541b2ccd425af
unknown <LucasFeliciano02@outlook.com>
2022-07-23 08:39:53 -0300

    Arquivo separado para validações de senha forte e de envio de email local

---
+++ autenticacao/utils.py
@@ -0,0 +1,46 @@
+import re
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+
+
+# Função validando senha forte
+def password_is_valid(request, password, confirm_password):
+    if len(password) < 6:
+        messages.add_message(request, constants.ERROR, 'Sua senha deve conter 6 ou mais caractertes')
+        return False
+
+    if not password == confirm_password:
+        messages.add_message(request, constants.ERROR, 'As senhas não coincidem!')
+        return False
+    
+    if not re.search('[A-Z]', password):
+        messages.add_message(request, constants.ERROR, 'Sua senha não contem letras maiúsculas')
+        return False
+    
+    if not re.search('[a-z]', password):
+        messages.add_message(request, constants.ERROR, 'Sua senha não contem letras minúsculas')
+        return False
+    
+    if not re.search('[1-9]', password):
+        messages.add_message(request, constants.ERROR, 'Sua senha não contém números')
+        return False
+    
+    return True
+
+
+# Função enviar email e criar pasta html em templates da pasta autentificação
+def email_html(path_template: str, assunto: str, para: list, **kwargs) -> dict:
+    
+    html_content = render_to_string(path_template, kwargs)  # Renderiza o arquivo html em algo que o email entenda
+    text_content = strip_tags(html_content)  # Remove as tags html
+    
+    email = EmailMultiAlternatives(assunto, text_content, settings.EMAIL_HOST_USER, para)  # Transforma tudo em algo que o django consiga entender
+    
+    email.attach_alternative(html_content, "text/html")  
+    email.send()
+    return {'status': 1}  
