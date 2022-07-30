80eeab728320fbab7b6d1022775fe0b425922281
unknown <LucasFeliciano02@outlook.com>
2022-07-30 08:08:16 -0300

    admin

---
+++ plataforma/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Pacientes, DadosPaciente, Refeicao, Opcao
+
+
+admin.site.register(Pacientes)
+admin.site.register(DadosPaciente)
+
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
